f86a5ae2247706322bac07ea3db47d09a6a56c39
Toms <tomas.manuel.vazquez@gmail.com>
2018-11-17 20:08:32 -0300

    v 0.1.1 navigation view

--- app/src/main/java/com/example/toms/recetarioamedida/view/MainActivity.java
+++ app/src/main/java/com/example/toms/recetarioamedida/view/MainActivity.java
@@ -5,11 +5,15 @@ import android.net.Uri;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -39,7 +43,8 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
-
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onResume() {
@@ -85,13 +90,57 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         imageView = findViewById(R.id.imagenFacebook);
 
+        //NavigationView
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigation);
 
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                drawerLayout.closeDrawers();
+
+                switch (menuItem.getItemId()){
+
+                    case R.id.login:
+                        if (currentUser != null){
+                            FirebaseAuth.getInstance().signOut();
+                            LoginManager.getInstance().logOut();
+                            navigationView.getMenu().findItem(R.id.login).setTitle(R.string.login_iniciar);
+                            currentUser = null;
+                        }else{
+                            cargarFragment(logInFragment);
+                        }
+                        return true;
+
+                    case R.id.recetas:
+                        //cargar fragments
+                        return true;
+
+                    case R.id.aboutUs:
+                        //cargar fragments
+                        return true;
+                }
+
+                return false;
+            }
+        });
 
 
     }
 
 
     //Metodos
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(Gravity.START)){
+            drawerLayout.closeDrawers();
+        }else{
+            super.onBackPressed();
+        }
+    }
+
     public void cargarFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -136,6 +185,8 @@ public class MainActivity extends AppCompatActivity {
             //String name = user.getDisplayName();
             Uri uri = user.getPhotoUrl();
             Glide.with(this).load(uri).into(imageView);
+            navigationView.getMenu().findItem(R.id.login).setTitle(R.string.login_salir);
+
         }else {
             //cargarFragment(logInFragment);
         }
--- app/src/main/java/com/example/toms/recetarioamedida/view/fragment/LogInFragment.java
+++ app/src/main/java/com/example/toms/recetarioamedida/view/fragment/LogInFragment.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -82,6 +83,14 @@ public class LogInFragment extends Fragment {
             public void onError(FacebookException exception) {
                 // App code
 
+            }
+        });
+
+        Button button = view.findViewById(R.id.continuarSinLogIn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().remove(LogInFragment.this).commit();
             }
         });
 
